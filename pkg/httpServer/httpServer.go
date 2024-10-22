3a20f670a62c87bfc72a272f5e4bb731f4479b9b
Nolan Jannotta <anjannotta@verizon.net>
2024-10-22 10:02:21 -0700

    ip distance

--- pkg/httpServer/httpServer.go
+++ pkg/httpServer/httpServer.go
@@ -6,12 +6,10 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
-	"math"
 	"net/http"
 	"net/mail"
 	"net/smtp"
 	"os"
-	"strconv"
 
 	"github.com/charmbracelet/log"
 )
@@ -48,7 +46,6 @@ func SetUp() *http.Server {
 		w.Write([]byte("HEALTHY"))
 	})
 	mux.HandleFunc("POST /email", handleEmail)
-	mux.HandleFunc("GET /ipdistance", handleIpAddressDistance)
 	// mux.HandleFunc("GET /{page}", subPageHandler)
 
 	s := &http.Server{
@@ -116,63 +113,4 @@ func handleEmail(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-}
-
-type Location struct {
-	Lat float64 `json:"lat"`
-	Lon float64 `json:"lon"`
-}
-type Response struct {
-	Miles string `json:"miles"`
-}
-type ClientIP struct {
-	IP string `json:"ip"`
-}
-
-func checkAndReturnServerError(w http.ResponseWriter, err error) {
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-func handleIpAddressDistance(w http.ResponseWriter, r *http.Request) {
-	IPDistanceURL := "http://ip-api.com/json/"
-	laLat, laLon := 34.052235, -118.243683 // latitude and longitude of Los Angeles
-
-	// get clients public IPv4 address
-	var clientIP ClientIP
-	cIP, err := http.Get("https://api.ipify.org?format=json")
-	checkAndReturnServerError(w, err)
-	err = json.NewDecoder(cIP.Body).Decode(&clientIP)
-	checkAndReturnServerError(w, err)
-
-	// latitude and longitude of clients IP address
-	locationResp, err := http.Get(fmt.Sprint(IPDistanceURL, clientIP.IP))
-	checkAndReturnServerError(w, err)
-	var location Location
-	err = json.NewDecoder(locationResp.Body).Decode(&location)
-	checkAndReturnServerError(w, err)
-
-	// calculate distance between lats and lons
-	distance := getDistance(laLat, laLon, location.Lat, location.Lon)
-
-	response := Response{Miles: strconv.Itoa(int(distance * 0.621371))}
-
-	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode(response)
-
-}
-
-func getDistance(lat1, lon1, lat2, lon2 float64) float64 {
-	// Convert degrees to radians
-	lat1 = lat1 * math.Pi / 180
-	lon1 = lon1 * math.Pi / 180
-	lat2 = lat2 * math.Pi / 180
-	lon2 = lon2 * math.Pi / 180
-
-	// Haversine formula to calculate the great-circle distance
-	distance := math.Acos(math.Sin(lat1)*math.Sin(lat2)+math.Cos(lat1)*math.Cos(lat2)*math.Cos(lon2-lon1)) * 6371
-
-	return distance
 }
